05e69899b1aef6e6056852221c346e5c3b80cb90
zuimrs <zuimrs@gmail.com>
2018-01-04 18:40:44 +0800

    :construction: update detail.html and fix some bug

--- project/dashboard/routes.py
+++ project/dashboard/routes.py
@@ -27,7 +27,7 @@ def home(userid):
 	# 			 for ins in json.loads(data)['ins_list']]
 	#  ==========================================================================
 
-	# WARNING: This is an example for the value of api_client function,need to be replaced;
+	# WARNING: This is an example for the api_client function,need to be replaced;
 	# ===========================================================================
 	ins_list = [dict(name = "test.name1", id = 1, is_deleted = False, update_time = 100)]
 	# ===========================================================================
@@ -39,14 +39,24 @@ def home(userid):
 @login_required
 def detail(iid):
 	login_data = dict(name = current_user.name, id = current_user.id)
-	api_client = ApiClient(login_data=login_data)
-	api_client.login()
-	data = api_client.get_instance_detail(iid=iid)
-	#具体返回参数看 k8sAPI！！！
-	#交给前端解析，注意区分 id 和 uid，实际上用户查询更新删除依赖的是 id 而非 uid
-	param = json.loads(data) # 应该是dict
-	return render_template("dashboard/instance/detail.html",
-							param = param)
+	#  TODO: replace debug code with api_client
+	#
+	#  ==========================================================================
+	# api_client = ApiClient(login_data=login_data)
+	# api_client.login()
+	# data = api_client.get_instance_detail(iid=iid)
+	#  ==========================================================================
+
+	# WARNING: This is an example for the api_client function,need to be replaced;
+	#  ==========================================================================
+	data = dict(id = 1,name = "instance_test_name",\
+		CPUsize = 10,MEMsize = 1024.0,GPUnum = 10,scale = 10,\
+		isSSD = True,postStamp = 10000,serviceIP = "127.0.0.1",\
+		servicePort = 10086,updateStamp = 10020,deleteStamp = 20000)
+	#  ==========================================================================
+
+	return render_template("dashboard/detail.html",
+							userid = current_user.id,iid = iid,data = data)
 
 @dashboard.route('/create', methods=['GET','POST'])
 @login_required
@@ -69,7 +79,7 @@ def create():
 		# 	return redirect(url_for('.home'))
 		# =============================================================================
 
-	return render_template('dashboard/create.html', form=form)
+	return render_template('dashboard/create.html',userid = current_user.id, form=form)
 
 @dashboard.route('/update/<int:iid>', methods=['POST', 'GET'])
 @login_required
@@ -77,24 +87,41 @@ def update(iid):
 	form = UpdateForm()
 	if form.validate_on_submit():
 		login_data = dict(name = current_user.name, id = current_user.id)
-		api_client = ApiClient(login_data=login_data)
-		api_client.login()
-		r_status = api_client.update_instance(iid=iid, new_scale = form.new_scale.data)
-		if r_status == 200:
-			flash('updated')
-			return redirect(url_for('.home'))
-	return render_template('dashboard/update.html', form=form)
+		# TODO: replace debug code with api_client
+		# =============================================================================
+		# api_client = ApiClient(login_data=login_data)
+		# api_client.login()
+		# r_status = api_client.update_instance(iid=iid, new_scale = form.new_scale.data)
+		# if r_status == 200:
+		# 	flash('updated')
+		# 	return redirect(url_for('.home'))
+		# =============================================================================
 
-@dashboard.route('/delete/<int:iid>', methods=['POST', 'GET'])
+		# WARNING: This is an example for the api_client function,need to be replaced;
+		#  ==========================================================================
+
+		#  ==========================================================================
+
+	return render_template('dashboard/update.html',userid = current_user.id, iid = iid, form=form)
+
+@dashboard.route('/delete/<int:iid>', methods=['POST'])
 @login_required
 def delete(iid):
 	form = DeleteForm()
 	if form.validate_on_submit():
 		login_data = dict(name = current_user.name, id = current_user.id)
-		api_client = ApiClient(login_data=login_data)
-		api_client.login()
-		r_status = api_client.delete_instance(iid=iid)
-		if r_status == 200:
-			flash('deleted')
-			return redirect(url_for('.home'))
-	return render_template('dashboard/delete.html', form=form)
+		# TODO: replace debug code with api_client
+		# =============================================================================
+		# api_client = ApiClient(login_data=login_data)
+		# api_client.login()
+		# r_status = api_client.delete_instance(iid=iid)
+		# if r_status == 200:
+		# 	flash(str('instance ID = '  + str(iid)  + ' has been deleted'))
+		# 	return redirect(url_for('.home',userid = current_user.id))
+		# =============================================================================
+
+	# WARNING: This is an example for the api_client function,need to be replaced;
+	#  ==========================================================================
+	flash(str('instance ID = '  + str(iid)  + ' has been deleted'))
+	return redirect(url_for('.home',userid = current_user.id))
+	#  ==========================================================================
